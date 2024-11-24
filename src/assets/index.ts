c85a5ba26b59685a74d7e88abfacf616506e8769
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-24 23:48:52 +0100

    first commit

---
+++ src/assets/index.ts
@@ -0,0 +1,17 @@
+//  importando todos os logos nos meus assets
+
+import logo_new from './logos/new-logo.png'
+
+
+// agrupando os logos em um objeto
+export const logos = {
+    logo_new
+}
+
+
+
+// exportando todas as imagens em assets
+
+export const images = {
+    logos
+}   
